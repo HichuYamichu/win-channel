caf85e4ccfcb56f813b7246083e973889d285713
HichuYamichu <hichuyamichu@gmail.com>
2025-04-23 09:57:04 +0200

    rewrite; switched internals to use sync queue now

--- readme.md
+++ readme.md
@@ -1,29 +1,24 @@
 # WinChannel
 
-WinChannel is a Rust channel implementation backed by Windows shared memory and synchronization primitives. It's useful if you need to pass data and synchronize between multiple processes that you do not own (for example when foreign process loads your dll and you need to signal some events back to your main exe).
+A channel-like API allowing you to send messages not only between different threads but also between processes. Given the nature of the problem, Senders and Receivers are created separately and have to have matching configurations; this cannot be enforced by the compiler, so the user has to make sure to uphold numerous safety requirements.
 
-# Requirements and design considerations
+# Safety requirements
 
-Since creating global Windows events and shared memory requires special privileges, in order to use this library correctly process that creates shared structures needs to be run with administrator privileges.
+- This is MPSC channel; there can be at most one consumer/receiver at any given time.
+- WinChannel is generic; thus, the user MUST make sure that every instance connected through the same shared memory block uses exactly the same generic parameters.
+- Queue size MUST be a multiple of 2.
+- You CANNOT send any type with internal pointers or references or anything dependent on your address space.
+- Hung-up processes might prevent cleanup of kernel objects; thus, it is highly recommended to not reuse the same configurations for new instances after tearing down previous ones, or you might connect to leftover shmem.
+- Once the shutdown message is received, you should drop your instance, or it will prevent a full cleanup.
 
-If shared queue is full incoming push will fail and message will be lost (adjust the queue size and make sure a receiving end does not fall behind).
+# Features overview
 
-Neither Sender nor Receiver _directly_ allocate/manage any dynamic memory. 
+- Independent creation—you do not need to synchronize creation of Senders/Receivers; the first one to be created will initialize required structures.
+- Blocking and non-blocking API—non-blocking functions fail when there's not enough space inside the shared queue.
+- Manual teardown—you can send a shutdown signal to every connected instance; this is not guaranteed to actually release any resources. A hung-up process might prevent complete cleanup; in any case, process termination releases all handles held by that process; therefore, all kernel objects can eventually be freed.
 
-This is not high level library, Windows types are used as both inputs and results of functions (You need to depend on windows or windows-core crate).
+# Implementation
 
-# Safety
+- WinChannel is a fairly standard Sync Queue. Both Senders and Receivers share a Ring Buffer with atomic head/tail indices. A CAS loop is used for element insertion, while an atomic flag is used for safe handoff (which is sufficient under the assumption that there's only one receiver).
 
-There are numerous safety requirements that need to be upheld by the user. Most importantly:
-
-- There must exist at most one Receiver.
-- Receiver must be created before any Sender.
-- Since Sender and Receiver halves are created independently caller must ensure their configs match exactly.
-- Types that allocate internally CANNOT be passed by this channel (obviously as they are going to allocate inside address space of current process so receiving end will most likely access unmapped memory regions).
-- Drop is not run for internal queue elements.
-
-# TODO
-
-- Timing independent Sender/Receiver (first created structure should be responsible for creating shared memory).
-- More optimal implementation (currently it's a standard channel implementation with mutex protected queue and condvar (Windows Event Object)).
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,213 +1,351 @@
-use std::collections::VecDeque;
-use std::ffi::CStr;
-use std::mem::MaybeUninit;
+use std::{
+    array,
+    cell::UnsafeCell,
+    fmt,
+    mem::MaybeUninit,
+    sync::atomic::{AtomicBool, AtomicUsize, Ordering},
+};
 
-use windows::core::Result as WinResult;
-use windows::core::PCSTR;
-use windows::Win32::Foundation::*;
-use windows::Win32::Security::*;
-use windows::Win32::System::Memory::*;
-use windows::Win32::System::SystemServices::SECURITY_DESCRIPTOR_REVISION;
-use windows::Win32::System::Threading::*;
-use windows::Win32::System::WindowsProgramming::*;
+use windows::{
+    core::{Error, Result as WinResult, PCWSTR},
+    Win32::Foundation::*,
+    Win32::System::{Memory::*, Threading::*},
+};
 
-pub struct Config<'a> {
-    pub shmem_name: &'a CStr,
-    pub mutex_name: &'a CStr,
-    pub event_name: &'a CStr,
+// NOTE: SYNCHRONIZE constant is missing from windows-rs crate.
+const SYNCHRONIZE: SYNCHRONIZATION_ACCESS_RIGHTS = SYNCHRONIZATION_ACCESS_RIGHTS(0x00100000);
+
+pub enum TrySendError<T> {
+    Full(T),
+    Disconnected(T),
+    Other(Error),
 }
 
-pub struct WinSender<const S: usize, T> {
-    mem: *mut ArrayQueue<S, T>,
-    mutex: HANDLE,
-    send_event: HANDLE,
-}
-
-// SAFETY:
-// Access to raw pointers is synchronized with windows mutex and windows events.
-// No references are given out from this type.
-unsafe impl<const S: usize, T: Sync> Sync for WinSender<S, T> {}
-unsafe impl<const S: usize, T: Send> Send for WinSender<S, T> {}
-
-impl<const S: usize, T> WinSender<S, T> {
-    pub unsafe fn new(config: Config<'_>) -> WinResult<Self> {
-        let file = OpenFileMappingA(
-            FILE_MAP_READ.0 | FILE_MAP_WRITE.0,
-            FALSE,
-            PCSTR(config.shmem_name.as_ptr() as _),
-        )?;
-
-        let queue_size = std::mem::size_of::<ArrayQueue<S, T>>();
-        let mem = MapViewOfFile(file, FILE_MAP_ALL_ACCESS, 0, 0, queue_size).Value as *mut _
-            as *mut ArrayQueue<S, T>;
-
-        const SYNCHRONIZE: SYNCHRONIZATION_ACCESS_RIGHTS =
-            SYNCHRONIZATION_ACCESS_RIGHTS(0x00100000);
-
-        let mutex = OpenMutexA(SYNCHRONIZE.0, FALSE, PCSTR(config.mutex_name.as_ptr() as _));
-        let send_event = OpenEventA(
-            SYNCHRONIZE | EVENT_MODIFY_STATE,
-            FALSE,
-            PCSTR(config.event_name.as_ptr() as _),
-        )?;
-
-        Ok(Self {
-            mem,
-            mutex,
-            send_event,
-        })
-    }
-
-    pub unsafe fn send(&self, item: T) -> WinResult<()> {
-        WaitForSingleObject(self.mutex, INFINITE);
-
-        // SAFETY: We are the only thread with access to this memory at this point.
-        let queue = &mut *self.mem;
-        // Ignore failed push.
-        let _ = queue.push_back(item);
-
-        ReleaseMutex(self.mutex)?;
-        SetEvent(self.send_event)?;
-        Ok(())
+impl<T> fmt::Debug for TrySendError<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("TrySendError").finish_non_exhaustive()
     }
 }
 
-pub struct WinReceiver<const S: usize, T> {
-    mem: *mut ArrayQueue<S, T>,
-    mutex: HANDLE,
-    send_event: HANDLE,
-
-    local_queue: VecDeque<T>,
+pub enum SendError<T> {
+    Disconnected(T),
+    Other(Error),
 }
 
-impl<const S: usize, T> WinReceiver<S, T> {
-    pub unsafe fn new(config: Config<'_>) -> WinResult<Self> {
-        let mut sd: SECURITY_DESCRIPTOR = std::mem::zeroed();
-        let psd = PSECURITY_DESCRIPTOR(&mut sd as *mut _ as *mut _);
-
-        InitializeSecurityDescriptor(psd, SECURITY_DESCRIPTOR_REVISION)?;
-        SetSecurityDescriptorDacl(psd, TRUE, None, FALSE)?;
-
-        let sa = SECURITY_ATTRIBUTES {
-            nLength: std::mem::size_of::<SECURITY_ATTRIBUTES>() as _,
-            lpSecurityDescriptor: psd.0,
-            bInheritHandle: FALSE,
-        };
-
-        let queue_size = std::mem::size_of::<ArrayQueue<S, T>>();
-        let file = CreateFileMappingA(
-            INVALID_HANDLE_VALUE,
-            Some(&sa as *const _),
-            PAGE_READWRITE,
-            0,
-            queue_size as _,
-            PCSTR(config.shmem_name.as_ptr() as _),
-        )?;
-
-        let mem = MapViewOfFile(file, FILE_MAP_ALL_ACCESS, 0, 0, queue_size).Value
-            as *mut ArrayQueue<S, T>;
-        let queue = ArrayQueue::<S, T>::new();
-        std::ptr::write(mem, queue);
-
-        let mutex = CreateMutexA(
-            Some(&sa as *const _),
-            FALSE,
-            PCSTR(config.mutex_name.as_ptr() as _),
-        )?;
-
-        let send_event = CreateEventA(
-            Some(&sa as *const _),
-            FALSE,
-            FALSE,
-            PCSTR(config.event_name.as_ptr() as _),
-        )?;
-
-        Ok(Self {
-            mem,
-            send_event,
-            mutex,
-            local_queue: VecDeque::new(),
-        })
+impl<T> fmt::Debug for SendError<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("SendError").finish_non_exhaustive()
     }
+}
 
-    pub unsafe fn recv(&mut self) -> WinResult<T> {
-        match self.local_queue.pop_front() {
-            Some(item) => return Ok(item),
-            None => {}
-        }
+pub enum RecvError {
+    Disconnected,
+    Other(Error),
+}
 
-        loop {
-            WaitForSingleObject(self.send_event, INFINITE);
-            WaitForSingleObject(self.mutex, INFINITE);
-
-            // SAFETY: We are the only thread with access to this memory at this point.
-            let queue = &mut *self.mem;
-
-            while let Some(item) = queue.pop_front() {
-                self.local_queue.push_back(item);
-            }
-
-            ReleaseMutex(self.mutex)?;
-            ResetEvent(self.send_event)?;
-
-            match self.local_queue.pop_front() {
-                Some(message) => return Ok(message),
-                None => {} // Race happend and other Receiver stolen all messages from shared queue.
-            }
-        }
+impl fmt::Debug for RecvError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("RecvError").finish_non_exhaustive()
     }
 }
 
 #[derive(Debug)]
-struct ArrayQueue<const S: usize, T> {
-    data: [MaybeUninit<T>; S],
-    start: usize,
-    end: usize,
-    count: usize,
+struct Queue<T, const SIZE: usize> {
+    head: AtomicUsize,
+    tail: AtomicUsize,
+    slots: [Slot<T>; SIZE],
 }
 
-impl<const S: usize, T> ArrayQueue<S, T> {
+unsafe impl<T, const SIZE: usize> Send for Queue<T, { SIZE }> {}
+unsafe impl<T, const SIZE: usize> Sync for Queue<T, { SIZE }> {}
+
+#[derive(Debug)]
+struct Slot<T> {
+    value: UnsafeCell<MaybeUninit<T>>,
+    ready: AtomicBool,
+}
+
+impl<T, const SIZE: usize> Queue<T, { SIZE }> {
     fn new() -> Self {
         Self {
-            data: unsafe { MaybeUninit::uninit().assume_init() },
-            start: 0,
-            end: 0,
-            count: 0,
+            head: AtomicUsize::new(0),
+            tail: AtomicUsize::new(0),
+            slots: array::from_fn(|_| Slot {
+                value: UnsafeCell::new(MaybeUninit::uninit()),
+                ready: AtomicBool::new(false),
+            }),
         }
     }
 
-    fn is_empty(&self) -> bool {
-        self.count == 0
-    }
+    fn push(&self, value: T) -> Result<(), T> {
+        loop {
+            let head = self.head.load(Ordering::Acquire);
+            let tail = self.tail.load(Ordering::Acquire);
 
-    fn is_full(&self) -> bool {
-        self.count == S
-    }
+            if head.wrapping_sub(tail) >= SIZE {
+                // Queue is full.
+                return Err(value);
+            }
 
-    fn push_back(&mut self, item: T) -> Result<(), T> {
-        if self.is_full() {
-            return Err(item);
+            let new_head = head.wrapping_add(1);
+            if self
+                .head
+                .compare_exchange(head, new_head, Ordering::AcqRel, Ordering::Relaxed)
+                .is_ok()
+            {
+                let idx = head & (SIZE - 1); // Fast modulo for multiples of 2.
+                let slot = &self.slots[idx];
+
+                unsafe {
+                    (*slot.value.get()).write(value);
+                }
+
+                slot.ready.store(true, Ordering::Release);
+                return Ok(());
+            }
+
+            std::hint::spin_loop();
         }
-
-        // SAFETY: `end` is in range so our pointer write is valid.
-        unsafe {
-            self.data[self.end].as_mut_ptr().write(item);
-        }
-
-        self.end = (self.end + 1) % S;
-        self.count += 1;
-        Ok(())
     }
 
-    fn pop_front(&mut self) -> Option<T> {
-        if self.is_empty() {
+    // There has to be at most one Receiver.
+    unsafe fn pop(&self) -> Option<T> {
+        let tail = self.tail.load(Ordering::Acquire);
+        let head = self.head.load(Ordering::Acquire);
+        if tail == head {
+            // Queue is empty.
             return None;
         }
 
-        // SAFETY: There was write already so this memory is initialized.
-        let item = unsafe { self.data[self.start].as_ptr().read() };
-        self.start = (self.start + 1) % S;
-        self.count -= 1;
-        Some(item)
+        let slot = &self.slots[tail];
+        if !slot.ready.load(Ordering::Acquire) {
+            return None;
+        }
+
+        let value = unsafe { (*slot.value.get()).assume_init_read() };
+        slot.ready.store(false, Ordering::Release);
+        self.tail.fetch_add(1, Ordering::Release);
+        return Some(value);
+    }
+}
+
+fn create_or_map_shmem_queue<T, const SIZE: usize>(
+    file_mapping_name: PCWSTR,
+) -> Result<(MEMORY_MAPPED_VIEW_ADDRESS, HANDLE, bool), Error> {
+    let size = size_of::<Queue<T, { SIZE }>>();
+
+    let mapping = unsafe {
+        CreateFileMappingW(
+            INVALID_HANDLE_VALUE,
+            None,
+            PAGE_READWRITE,
+            0,
+            size as u32,
+            file_mapping_name,
+        )
+    };
+
+    let Ok(mapping) = mapping else {
+        return Err(Error::from_win32());
+    };
+
+    if mapping.0 == std::ptr::null_mut() {
+        return Err(Error::from_win32());
+    }
+
+    let existed = unsafe { GetLastError() } == ERROR_ALREADY_EXISTS;
+    let ptr = unsafe { MapViewOfFile(mapping, FILE_MAP_ALL_ACCESS, 0, 0, size) };
+    if ptr.Value.is_null() {
+        return Err(Error::from_win32());
+    }
+
+    return Ok((ptr, mapping, existed));
+}
+
+unsafe fn create_event(event_name: PCWSTR) -> WinResult<HANDLE> {
+    CreateEventW(None, false, false, event_name)
+}
+
+unsafe fn open_event(event_name: PCWSTR) -> WinResult<HANDLE> {
+    OpenEventW(SYNCHRONIZE | EVENT_MODIFY_STATE, false, event_name)
+}
+
+pub struct Config {
+    shmem_name: PCWSTR,
+    send_event_name: PCWSTR,
+    recv_event_name: PCWSTR,
+    disconnect_event_name: PCWSTR,
+}
+
+pub struct Sender<T, const SIZE: usize> {
+    shared_queue: *mut Queue<T, { SIZE }>,
+    mem_mapping: HANDLE,
+    item_added_event: HANDLE,
+    item_removed_event: HANDLE,
+    shutdown_event: HANDLE,
+}
+
+impl<T, const SIZE: usize> Sender<T, { SIZE }> {
+    pub unsafe fn new(config: Config) -> Result<Self, Error> {
+        let (shmem, mapping, existed) =
+            create_or_map_shmem_queue::<T, { SIZE }>(config.shmem_name)?;
+        let queue_ptr = shmem.Value as *mut Queue<T, { SIZE }>;
+
+        let s = match existed {
+            false => {
+                std::ptr::write(queue_ptr, Queue::new());
+                Self {
+                    shared_queue: queue_ptr,
+                    mem_mapping: mapping,
+                    item_added_event: create_event(config.send_event_name)?,
+                    item_removed_event: create_event(config.recv_event_name)?,
+                    shutdown_event: create_event(config.disconnect_event_name)?,
+                }
+            }
+            true => Self {
+                shared_queue: queue_ptr,
+                mem_mapping: mapping,
+                item_added_event: open_event(config.send_event_name)?,
+                item_removed_event: open_event(config.recv_event_name)?,
+                shutdown_event: open_event(config.disconnect_event_name)?,
+            },
+        };
+
+        Ok(s)
+    }
+
+    pub unsafe fn try_send(&self, value: T) -> Result<(), TrySendError<T>> {
+        let is_shutting_down = WaitForSingleObject(self.shutdown_event, 0) == WAIT_OBJECT_0;
+        if is_shutting_down {
+            return Err(TrySendError::Disconnected(value));
+        }
+        let res = unsafe { (*self.shared_queue).push(value) };
+        return res.map_err(|e| TrySendError::Full(e));
+    }
+
+    pub unsafe fn send(&self, mut value: T) -> Result<(), SendError<T>> {
+        while let Err(v) = (*self.shared_queue).push(value) {
+            value = v;
+            let wait_event = WaitForMultipleObjects(
+                &[self.shutdown_event, self.item_removed_event],
+                false,
+                INFINITE,
+            );
+
+            if wait_event == WAIT_FAILED {
+                return Err(SendError::Other(Error::from_win32()));
+            } else if wait_event.0 - WAIT_OBJECT_0.0 == 0 {
+                // 0 is the index of shutdown_event
+                return Err(SendError::Disconnected(value));
+            } else {
+            } // WAIT_TIMEOUT and WAIT_ABANDONED are not aplicable so this is the good case.
+        }
+        SetEvent(self.item_added_event).map_err(|e| SendError::Other(e))?;
+        return Ok(());
+    }
+
+    pub unsafe fn shutdown(&self) -> Result<(), Error> {
+        SetEvent(self.shutdown_event)
+    }
+}
+
+impl<T, const SIZE: usize> Drop for Sender<T, { SIZE }> {
+    fn drop(&mut self) {
+        unsafe {
+            let _ = CloseHandle(self.item_added_event);
+            let _ = CloseHandle(self.item_removed_event);
+            let _ = CloseHandle(self.shutdown_event);
+            let map = MEMORY_MAPPED_VIEW_ADDRESS {
+                Value: self.shared_queue as *mut _,
+            };
+            let _ = UnmapViewOfFile(map);
+            let _ = CloseHandle(self.mem_mapping);
+        }
+    }
+}
+
+pub struct Receiver<T, const SIZE: usize> {
+    shared_queue: *mut Queue<T, { SIZE }>,
+    mem_mapping: HANDLE,
+    item_added_event: HANDLE,
+    item_removed_event: HANDLE,
+    shutdown_event: HANDLE,
+}
+
+impl<T, const SIZE: usize> Receiver<T, { SIZE }> {
+    pub unsafe fn new(config: Config) -> Result<Self, Error> {
+        let (shmem, mapping, existed) =
+            create_or_map_shmem_queue::<T, { SIZE }>(config.shmem_name)?;
+        let queue_ptr = shmem.Value as *mut Queue<T, { SIZE }>;
+
+        let s = match existed {
+            false => {
+                std::ptr::write(queue_ptr, Queue::new());
+                Self {
+                    shared_queue: queue_ptr,
+                    mem_mapping: mapping,
+                    item_added_event: create_event(config.send_event_name)?,
+                    item_removed_event: create_event(config.recv_event_name)?,
+                    shutdown_event: create_event(config.disconnect_event_name)?,
+                }
+            }
+            true => Self {
+                shared_queue: queue_ptr,
+                mem_mapping: mapping,
+                item_added_event: open_event(config.send_event_name)?,
+                item_removed_event: open_event(config.recv_event_name)?,
+                shutdown_event: open_event(config.disconnect_event_name)?,
+            },
+        };
+
+        Ok(s)
+    }
+
+    pub unsafe fn try_recv(&self) -> Option<T> {
+        unsafe { (*self.shared_queue).pop() }
+    }
+
+    pub unsafe fn recv(&self) -> Result<T, RecvError> {
+        loop {
+            unsafe {
+                if let Some(item) = (*self.shared_queue).pop() {
+                    SetEvent(self.item_removed_event).map_err(|e| RecvError::Other(e))?;
+                    return Ok(item);
+                }
+
+                let wait_event = WaitForMultipleObjects(
+                    &[self.shutdown_event, self.item_added_event],
+                    false,
+                    INFINITE,
+                );
+
+                if wait_event == WAIT_FAILED {
+                    return Err(RecvError::Other(Error::from_win32()));
+                } else if wait_event.0 - WAIT_OBJECT_0.0 == 0 {
+                    // 0 is the index of shutdown_event
+                    return Err(RecvError::Disconnected);
+                } else {
+                } // WAIT_TIMEOUT and WAIT_ABANDONED are not aplicable so this is the good case.
+            }
+        }
+    }
+
+    pub unsafe fn shutdown(&self) -> Result<(), Error> {
+        SetEvent(self.shutdown_event)
+    }
+}
+
+impl<T, const SIZE: usize> Drop for Receiver<T, { SIZE }> {
+    fn drop(&mut self) {
+        unsafe {
+            let _ = CloseHandle(self.item_added_event);
+            let _ = CloseHandle(self.item_removed_event);
+            let _ = CloseHandle(self.shutdown_event);
+            let map = MEMORY_MAPPED_VIEW_ADDRESS {
+                Value: self.shared_queue as *mut _,
+            };
+            let _ = UnmapViewOfFile(map);
+            let _ = CloseHandle(self.mem_mapping);
+        }
     }
 }
